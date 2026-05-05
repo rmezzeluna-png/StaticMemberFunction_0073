92c0b30ed39f8ce2d11f9035b2a8b5387084de51
rmezzeluna-png <rmezzeluna@gmail.com>
2026-05-05 17:00:39 +0700

    static functtion

---
+++ seven.cpp
@@ -0,0 +1,58 @@
+//staticfunction
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim){
+        nim = pnim;
+    };
+
+    static int getNim(){
+        return nim;
+    }
+
+    mahasiswa(string pnama) :nama(pnama){
+        setID();
+    };
+};
+
+int mahasiswa::nim=0;
+
+void mahasiswa::setID(){
+    id = ++nim;
+}
+
+void mahasiswa::printAll(){
+    cout << "ID = " << id << endl;
+    cout << "nama = " << nama << endl;
+    cout << endl;
+}
+
+int main() {
+    mahasiswa mhs1("komet");
+    mahasiswa mhs2("bujang");
+    mahasiswa::setNim(9);
+    mahasiswa mhs3("agus");
+    mahasiswa mhs4("amir");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar objek = " << mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
