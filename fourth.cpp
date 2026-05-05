8cb9040affd54e8dad300fddcc88a684f6c3879a
rmezzeluna-png <rmezzeluna@gmail.com>
2026-05-05 16:13:55 +0700

    class

--- fourth.cpp
+++ fourth.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class Buku {
+    string judul;
+    public:
+        string setget(string judul) {
+            this->judul = judul; // nilai parameter 'judul' untuk member variabel 'judul'
+            return this->judul; // return nilai member variabel 'judul'
+        }
+};
